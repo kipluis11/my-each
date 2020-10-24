08dd5a443e66628e27a00d309ca3f65cefa1da9c
kip_parcell <kipluis@gmail.com>
2020-10-24 16:13:00 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+collection = []
+
+def my_each(collection)
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+
+    collection
+  else
+    return "You knocked the block off!"
+  end
+   
 end
